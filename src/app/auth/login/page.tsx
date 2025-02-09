87d0a601fe171d16e91e1d6a2294d69ad0b63bb0
Denis Klimov <tyferly2003@gmail.com>
2025-02-09 15:29:01 +0400

    fix

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -40,7 +40,7 @@ const Login = () => {
         <div className="min-h-screen bg-[#F2F2F2] flex items-center justify-center">
             <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
                 <h1 className="text-2xl font-bold text-center text-[#9305F2] mb-4">Авторизация</h1>
-                <form onSubmit={handleLogin}>
+                <form>
                     <div className="mb-4">
                         <label htmlFor="login" className="block text-sm font-medium text-[#333]">Логин</label>
                         <input
@@ -66,7 +66,7 @@ const Login = () => {
                     {error && <div className="text-red-500 text-sm mb-4">{error}</div>}
                     <div className="flex justify-center">
                         <button
-                            type="submit"
+                            onClick={handleLogin}
                             className="px-4 py-2 bg-[#9305F2] text-white rounded-md hover:bg-[#C763F2]"
                         >
                             Войти
